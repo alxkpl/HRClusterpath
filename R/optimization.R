f60e1d6ed977b973c44d4fa2200158783622be8f
alxkpl <alexandre.capel@umontpellier.fr>
2025-08-28 11:15:40 +0200

    Documentation of get_clusters and HR_Clusterpath functions

--- R/optimization.R
+++ R/optimization.R
@@ -1,80 +1,51 @@
-#' Step for the gradient descent
+#' Clusterpath algorithm for Hüsler-Reiss models
 #'
-#' @param gamma a d x d matrix : the variogram matrix.
-#' @param weights a d x d symmetric matrix with a zero diagonal.
-#' @param lambda a positive number : the weight of the penalty.
-#' @param size_grid integer : size of the search grid for the optimal step.
+#' Gradient descent based on the Clusterpath algorithm adapted to the likelihood
+#' of a graphical Hüsler-Reiss model. Usefull when the precision matrix \eqn{\Theta}
+#' (or the variogram \eqn{\Gamma}) has a block matrix structure.
 #'
-#' @returns A function of clusters and R matrix which returns the next step of
-#' the optimisation for the gradient descent algorithm.
+#' The block matrix models is defined from two elements : the cluster's partition
+#' \eqn{\{C_1, \dots, C_K\}}, included in \eqn{V} and the \eqn{R} matrix which belongs
+#' to \eqn{\mathcal S_K(\mathbb R)}, the set of symmetric \eqn{K \times K} matrix (See also
+#' \code{\link{build_theta}()} and \code{\link{extract_R_matrix}()}).
 #'
-#' @examples
-#' R <- matrix(c(0.5, -1,
-#'               -1, -1), nr = 2)
-#' clusters <- list(c(1, 3), c(2,4))
-#' W <- matrix(c(0, 1, 1, 1,
-#'               1, 0, 1, 1,
-#'               1, 1, 0, 1,
-#'               1, 1, 1, 0), nc = 4)
-#' gamma <- matrix(c(0, 2, 1, 0,
-#'                   2, 0, 4, 1,
-#'                   1, 4, 0, 7,
-#'                   0, 1, 7, 0), nc = 4)
-#' f <- step_gradient(gamma, W, 0.5)
-#' f(R, clusters, 1)
+#' The Clusterpath aims to find optimum of some penalised negative loglikelihood defined in
+#' \code{\link{neg_likelihood_pen}()}.
 #'
-#' @keywords internal
-step_gradient <- function(gamma, weights, size_grid = 100) {
-  # Initialization of functions
-  dlog <- nloglike_grad_np(gamma)                     # Neg-lklh gradient part
-  dpen <- penalty_grad(weights)                       # Penalty gradient part
-
-  # Penalised negative log-likelihood
-  nllh <- neg_likelihood_pen(gamma, weights)
-
-  function(R, clusters, lambda) {
-    # Initialization
-    p <- sapply(clusters, length)           # Vector of cluster's size
-
-    # Gradient matrix computation
-    grad <- dlog(R, clusters) + lambda * dpen(R, clusters)
-
-    # Grid line search for optimal gradient step
-    # Grid line construction
-    check_pos <- \(.) !semi_def(sub_theta(R - . * grad, clusters))
-
-    if (max(p) == 1) {
-      s_opt <- optim(par = 1, fn = \(.) nllh(R - . * grad, clusters, lambda),
-                     method = "Brent", lower = 0, upper = 1)$par
-
-      s_opt <- s_optimal(s_opt, check_pos)
-
-      return(list(step = s_opt, gradient = grad))
-    }
-    s_max <- min(
-      # Maximum step size to get positive matrix
-      abs(((R %*% p) / (grad %*% p))[p > 1])
-    )
-
-    s_opt <- optim(par = 1, fn = \(.) nllh(R - . * grad, clusters, lambda),
-                   method = "Brent", lower = 0, upper = min(s_max, 1))$par
-
-    s_opt <- s_optimal(s_opt, check_pos)
-    # Returning results : size step and gradient matrix
-    list(step = s_opt, gradient = grad)
-  }
-}
-
-#' Gradient descent algorithm for Husler-Reiss graphical models clustering
+#' @name hr-clusterpath
 #'
-#' @param gamma a d x d matrix : the variogram matrix.
-#' @param weights a d x d symmetric matrix with a zero diagonal.
-#' @param lambda a positive number : the weight of the penalty.
-#' @param ...
+#' @param lambda For `get_clusters()`, a positive number : the weight of the penalty.
 #'
-#' @returns A function which compute the maximum likelihood estimator using
-#' cluster-path gradient descent and returns the estimation of the clusters and
-#' the corresponding R matrix.
+#' For `HR_Clusterpath()`, a numerix vector of positive number : the grid line for \eqn{\lambda}.
+#'
+#' @param eps_f A positive number : tolerance threshold for merging clusters.
+#' @param eps_g A positive number : tolerance threshold for the convergence of the gradient descent.
+#' @param it_max An integer : maximal number of iteration of the gradient descent algorithm.
+#'
+#' @returns `HR_Clusterpath()` is a gradient descent from the data with default values and method for
+#' the optimization : the variogram matrix \eqn{\Gamma} is the empirical variogram and the weights are
+#' set as the exponential weights, which depends of only one tuning parameter \eqn{\zeta} and where 
+#' we have some theoretical results.
+#'
+#' `get_clusters()` is a freer version of the previous function. You can use your own estimation for the
+#' variogram \eqn{\Gamma} and customizable weights with the matrix \eqn{W}.
+#'
+#' Both of them produce a list of results :
+#'  - $R : the \eqn{R} matrix of the clusters.
+#'  - $clusters : a list of the variable indices, clustered.
+#'  - $nllh : the value of the negative penalised negative loglikelihood.
+#'  - $lambda : the value of lambda.
+#'
+#' In the case of `HR_Clusterpath()` it is a list of the previous list.
+#'
+#' @section Some results for `HR_Clusterpath()`:
+#'
+#' When we get replications \eqn{X_1, \dots, X_n} of a random vector \eqn{X} which
+#' belongs to the attraction domain of a \eqn{K}-block Hüsler-Reiss graphical model, the
+#' minimum of the penalised negative loglikelihood conveges almost surely to the
+#' true precision matrix of the model \eqn{\Theta^*}, for all \eqn{\lambda}, provided that
+#' we choose a sequence \eqn{(\zeta_n)_{n\in \mathbb N^*}} which grows slower than the
+#' \eqn{log(n)} sequence. In general, the \eqn{\zeta \in [1,2]} is a good choice.
 #'
 #' @examples
 #' W <- matrix(c(0, 1, 1, 1,
@@ -89,8 +60,13 @@ step_gradient <- function(gamma, weights, size_grid = 100) {
 #' Cluster_HR <- get_cluster(gamma, W, 100)
 #' Cluster_HR(R)
 #'
-#' @export
+NULL
+
+#' @rdname hr-clusterpath
 #'
+#' @param gamma For `get_clusters()`, a \eqn{d \times d} matrix : the variogram matrix \eqn{\Gamma}.
+#' @param weights For `get_clusters()`, the \eqn{d \times d} symmetric weightsmatrix with a zero diagonal.
+#' @export
 get_cluster <- function(gamma, weights, eps_f, ...) {
   L <- neg_likelihood_pen(gamma, weights)
   step <- step_gradient(gamma, weights, ...)
@@ -144,17 +120,10 @@ get_cluster <- function(gamma, weights, eps_f, ...) {
   }
 }
 
-#' Optimization results from data
+#' @rdname hr-clusterpath
 #'
-#' @param data \eqn{n \times d} matrix : the data.
-#' @param zeta a positive number : tuned parameter for the exponential weights.
-#' @param lamda a numerix vector of positive number : the grid line for \eqn{\lambda}.
-#' @param eps_f d
-#' @param eps_g d
-#' @param it_max d
-#'
-#' @returns Returns the optimization path results from `get_clusters()` for each
-#' \eqn{\lambda} from the data.
+#' @param data For `HR_Clusterapth()`, a \eqn{n \times d} matrix : the matrix of data.
+#' @param zeta For `HR_Clusterapth()`, a positive number : tuned parameter for the exponential weights.
 #'
 #' @importFrom graphicalExtremes emp_vario Gamma2Theta
 #'
@@ -191,4 +160,59 @@ HR_Clusterpath <- function(data, zeta, lambda, eps_g = 1e-3, eps_f = 1e-2, it_ma
     .options = furrr::furrr_options(seed = TRUE)
   )
 
+}
+
+# internal ------------------------------------------------------------------------------------
+
+#' Step for the gradient descent
+#'
+#' @noRd
+#' @param gamma a d x d matrix : the variogram matrix.
+#' @param weights a d x d symmetric matrix with a zero diagonal.
+#' @param lambda a positive number : the weight of the penalty.
+#' @param size_grid integer : size of the search grid for the optimal step.
+#'
+#' @returns A function of clusters and R matrix which returns the next step of
+#' the optimisation for the gradient descent algorithm.
+#'
+#' @keywords internal
+step_gradient <- function(gamma, weights, size_grid = 100) {
+  # Initialization of functions
+  dlog <- nloglike_grad_np(gamma)                     # Neg-lklh gradient part
+  dpen <- penalty_grad(weights)                       # Penalty gradient part
+
+  # Penalised negative log-likelihood
+  nllh <- neg_likelihood_pen(gamma, weights)
+
+  function(R, clusters, lambda) {
+    # Initialization
+    p <- sapply(clusters, length)           # Vector of cluster's size
+
+    # Gradient matrix computation
+    grad <- dlog(R, clusters) + lambda * dpen(R, clusters)
+
+    # Grid line search for optimal gradient step
+    # Grid line construction
+    check_pos <- \(.) !semi_def(sub_theta(R - . * grad, clusters))
+
+    if (max(p) == 1) {
+      s_opt <- optim(par = 1, fn = \(.) nllh(R - . * grad, clusters, lambda),
+                     method = "Brent", lower = 0, upper = 1)$par
+
+      s_opt <- s_optimal(s_opt, check_pos)
+
+      return(list(step = s_opt, gradient = grad))
+    }
+    s_max <- min(
+      # Maximum step size to get positive matrix
+      abs(((R %*% p) / (grad %*% p))[p > 1])
+    )
+
+    s_opt <- optim(par = 1, fn = \(.) nllh(R - . * grad, clusters, lambda),
+                   method = "Brent", lower = 0, upper = min(s_max, 1))$par
+
+    s_opt <- s_optimal(s_opt, check_pos)
+    # Returning results : size step and gradient matrix
+    list(step = s_opt, gradient = grad)
+  }
 }
