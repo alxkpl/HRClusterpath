5074201b4eb215753ee18226b590b0278ea474ad
alxkpl <alexandre.capel@umontpellier.fr>
2025-09-26 09:35:28 +0200

    Correction infinite loop in optimization

--- src/loop_positive.cpp
+++ src/loop_positive.cpp
@@ -5,8 +5,10 @@ using namespace Rcpp;
 //[[Rcpp::export]]
 double s_optimal(double s, Function f) {
     double s_out = s;
-    while (as<bool>(f(s))) {
+    int cpt = 0;
+    while ((as<bool>(f(s))) && (cpt < 100)) {
         s_out *= 0.95;
+        cpt++;
     }
     return s_out;
 }
