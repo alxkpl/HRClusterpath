67e4a9954a66ae67f97f9b1618ef056bb8d1c271
alxkpl <alexandre.capel@umontpellier.fr>
2026-02-27 10:48:53 +0100

    Update doc for likelihood functions

--- R/likelihood.R
+++ R/likelihood.R
@@ -1,38 +1,47 @@
 #' Negative log-likelihood for Hüsler-Reiss graphical models
 #'
-#' The penalised negative loglikelihood of the Hüsler-Reiss graphical models can be
-#' written as follow :
+#' The penalised negative log-likelihood of the Hüsler-Reiss graphical models can be
+#' written as follow:
 #' \deqn{
 #'  L_{\mathcal P}^{(n)}(\Theta, \lambda) = \underbrace{-\log(|\Theta|_+) -
 #'  \frac 12 \text{tr}(\hat \Gamma^{(n)}\Theta)}_{L^{(n)}(\Theta)} + \lambda
-#'  \underbrace{\sum_{i<j} w_{ij} d^2_{ij}(\Theta)}_{\mathcal P(\Theta)}
+#'  \underbrace{\sum_{i<j} w_{ij} d^2_{ij}(\Theta)}_{\mathcal P(\Theta)},
 #' }
 #' where \eqn{|\cdot|_+} is the generalised determinant, \eqn{n} is the sample size,
 #' \eqn{\hat \Gamma^{(n)}} an estimation of the variogram matrix \eqn{\Gamma} and \eqn{w_{ij}>0} the weights.
 #'
-#' We use the block matrix structure to compute the likelihood here (see \code{\link{build_theta}()} and
-#' \code{\link{extract_R_matrix}()}), that is why we use two parameters for the computation
-#' instead of only one matrix \eqn{\Theta}.
+#' An equivalent version of the negative likelihood is used by the function which is more convenient
+#' for the optimization procedure. It is defined by the following:
+#'
+#' \deqn{
+#'  L^{(n)}(\Theta, \lambda) = -\log(|P^t \Theta P|) - \frac 12 \text{tr}(\Gamma \Theta),
+#' }
+#' where \eqn{P} is a matrix \eqn{d\times (d-1)} matrix such that \eqn{P P^t = \Pi}, with \eqn{\Pi}
+#' the projection matrix on the orthogonal space of \eqn{<\pmb{1}_d>}.
+#'
+#' The block matrix structure is used to compute the negative log-likelihood (see \code{\link{build_theta}()} and
+#' \code{\link{extract_R_matrix}()}) not directly the value of the precision matrix \eqn{\Theta}.
 #'
 #' @name likelihood
 #'
-#' @param gamma A \eqn{d \times d} matrix: the variogram matrix \eqn{\Gamma}.
-#' @param weights The \eqn{d \times d} symmetric matrix \eqn{W} with a zero diagonal.
+#' @param R A \eqn{K \times K} matrix : the matrix of the clusters.
 #'
-#' @returns For `neg_likelihood()`, it produces a function with two parameters :
+#' @param clusters A list of indices associated to a partition of \eqn{V}.
+#'
+#' @returns For `NegLikelihood_HR()`, it computes the value of the negative log-likelihood from two parameters:
 #' - **R** the \eqn{K \times K} clusters matrix
 #' - **clusters** a list of indices associated to a partition of \eqn{V}
 #' and compute the value of the non penalised negative log-likelihood \eqn{L^{(n)}(\Theta)}.
 #'
-#' For `penalty()`, it produces a function with two parameters :
+#' For `penalty()`, it computes the value of the penalty with:
 #' - **R** the \eqn{K \times K} clusters matrix
 #' - **clusters** a list of indices associated to a partition of \eqn{V}
 #' and compute the value of the penalty \eqn{\mathcal P(\Theta)}.
 #'
-#' For `neg_likelihood_pen()`, it produces a function with three parameters :
+#' For `NegLikelihood_penalised()`, it computes the value of the penalised negative log-likelihood with:
 #' - **R** : the \eqn{K \times K} clusters matrix
 #' - **clusters** : a list of indices associated to a partition of \eqn{V}
-#' - **lambda** : a positive number, the weight of the penalty.
+#' - **lambda** : a positive number, the regularized parameter.
 #' and compute the value of the penalised negative log-likelihood \eqn{L_{\mathcal P}^{(n)}(\Theta, \lambda)}.
 #'
 #' @examples
@@ -78,6 +87,8 @@ NULL
 
 #' @rdname likelihood
 #'
+#' @param Gamma A \eqn{d \times d} matrix: the variogram matrix \eqn{\Gamma}.
+#'
 #' @export
 NegLikelihood_HR <- function(R, clusters, Gamma) {
   d <- ncol(Gamma)
@@ -90,6 +101,8 @@ NegLikelihood_HR <- function(R, clusters, Gamma) {
 
 #' @rdname likelihood
 #'
+#' @param weights The \eqn{d \times d} symmetric matrix \eqn{W} with a zero diagonal.
+#'
 #' @export
 penalty <- function(R, clusters, weights) {
   return(
@@ -98,6 +111,10 @@ penalty <- function(R, clusters, weights) {
 }
 
 #' @rdname likelihood
+#'
+#' @param Gamma A \eqn{d \times d} matrix: the variogram matrix \eqn{\Gamma}.
+#' @param weights The \eqn{d \times d} symmetric matrix \eqn{W} with a zero diagonal.
+#' @param lambda A positive number, the regularized parameter.
 #'
 #' @export
 NegLikelihood_penalised <- function(R, clusters, Gamma, weights, lambda) {
